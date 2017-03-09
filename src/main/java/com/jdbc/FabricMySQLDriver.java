d4508cf12fbe1bf355fcab3eda1fe9679d88b238
Araik <Araik>
2017-03-09 20:09:37 +0400

    this is first commit

---
+++ src/main/java/com/jdbc/FabricMySQLDriver.java
@@ -0,0 +1,41 @@
+package com.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverPropertyInfo;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+/**
+ * Created by Araik on 3/9/2017.
+ */
+public class FabricMySQLDriver implements java.sql.Driver {
+    public Connection connect(String url, Properties info) throws SQLException {
+        return null;
+    }
+
+    public boolean acceptsURL(String url) throws SQLException {
+        return false;
+    }
+
+    public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
+        return new DriverPropertyInfo[0];
+    }
+
+    public int getMajorVersion() {
+        return 0;
+    }
+
+    public int getMinorVersion() {
+        return 0;
+    }
+
+    public boolean jdbcCompliant() {
+        return false;
+    }
+
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return null;
+    }
+}
---
+++ src/main/java/com/jdbc/Main.java
@@ -0,0 +1,42 @@
+package com.jdbc;
+
+import java.sql.*;
+
+public class Main {
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/mysql";
+
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = null;
+        Driver driver;
+        driver = new FabricMySQLDriver();
+
+        try{
+            DriverManager.registerDriver(driver);
+        }
+        catch (SQLException ex){
+            System.out.println("csdcsdfc");
+            return;
+
+        }
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            Statement statement = connection.createStatement(){
+
+            }
+        }
+
+        catch (SQLException ex){
+            System.out.println("ne udalos");
+            return;
+
+        }
+        finally {
+            if(connection !=null)
+                connection.close();
+    }
+
+
+}}
